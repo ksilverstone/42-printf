a74f4301cf46fdd839cb7a01b320aa56e0830940
ksilverstone <144620460+ksilverstone@users.noreply.github.com>
2024-11-26 19:31:01 +0300

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -3,25 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgumusta <kgumusta@student.42kocaeli.co    +#+  +:+       +#+        */
+/*   By: kgumusta <kgumusta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 10:42:49 by kgumusta          #+#    #+#             */
-/*   Updated: 2024/11/05 14:53:34 by kgumusta         ###   ########.fr       */
+/*   Updated: 2024/11/26 19:30:05 by kgumusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <unistd.h>
 
-int ft_check(char f)
+static int format_func(const char *format, va_list args)
 {
-	return (f == 'c' || f == 'd' || f == 'i' || f == 'u' || f == '%'
-		|| f == 's' || f == 'x' || f == 'X' || f == 'p');
-}
+    int char_count;
 
-int ft_format(va_list arr, char format)
-{
-    if(format == 'c')
-        return(ft_putchar(va_arg(arr, int)));
-    else if(format ==)
+    char_count = 0;
+    if (*format == 'c')
+		return (char_count += ft_putchar((char)va_arg(args, int)));
+	else if (c == '%')
+		return (write(1, "%", 1));
+	else if (c == 'd' || c == 'i')
+		return (ft_nbr(va_arg(args, int)));
+	else if (c == 'u')
+		return (ft_nbr(va_arg(args, unsigned int)));
+	else if (c == 's')
+		return (ft_string(va_arg(args, char *)));
+	else if (c == 'x' || c == 'X')
+		return (ft_hex(va_arg(args, unsigned int), c));
+	else if (c == 'p')
+		return (ft_point(va_arg(args, void *)));
+	else
+		return (-1);
+
 }
--- ft_printf_utilities.c
+++ ft_printf_utilities.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_utilities.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgumusta <kgumusta@student.42kocaeli.co    +#+  +:+       +#+        */
+/*   By: kgumusta <kgumusta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:33:56 by kgumusta          #+#    #+#             */
-/*   Updated: 2024/11/25 17:09:17 by kgumusta         ###   ########.fr       */
+/*   Updated: 2024/11/26 19:30:07 by kgumusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,68 @@ int ft_string(char *s)
     	return (write(1, "(null)", 6));
 	while (s[i] != '\0')
 	{
-		check = write (1, &(s[i++]), 1);
+		check = write(1, &(s[i++]), 1);
 		if (check == -1)
 			return (-1);
 	}
 	return (i);
 }
 
-int ft_int(int n)
+int	ft_putnbr(int n)
 {
-    
+	int char_count;
+
+	char_count = 0;
+	if(n == -2147483648)
+	{
+		if(ft_putchar("-2147483648") == -1)
+			return (-1);
+		return (11);
+	}
+	if(n < 0)
+	{
+		if(ft_putchar('-') == -1)
+			return (-1);
+		n *= -1;
+		char_count++;
+	}
+	if(n >= 10)
+		char_count += ft_putnbr(n / 10);
+	if(ft_putchar((n % 10) + '0') == -1)
+		return (-1);
+	return (++char_count);
+}
+
+int ft_putnbr_u(unsigned int n)
+{
+	int char_count;
+
+	char_count = 0;
+	if(n >= 10)
+		char_count += ft_putnbr_u(n);
+	if(ft_putchar((n % 10) + '0') == -1)
+		return (-1);
+	return (++char_count);
+}
+
+int	ft_hex(unsigned int i, char x)
+{
+	int char_count;
+
+	char_count = 0;
+	if (i >= 16)
+	{
+		char_count += ft_hex(i / 16, x);
+	}
+	if (x == 'x')
+	{
+		if(ft_putchar("0123456789abcdef"[i % 16]) == -1);
+			return (-1);
+	}
+	else if (x == 'X')
+	{
+		if(ft_putchar("0123456789abcdef"[i % 16]) == -1);
+			return (-1);
+	}
+	return (++char_count);
 }
